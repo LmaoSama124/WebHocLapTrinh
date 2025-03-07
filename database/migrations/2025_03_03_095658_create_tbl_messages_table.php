dcfe3dc32f6879d4853bd50e0abab27d6558a253
LmaoSama <lmaosama1242004@gmail.com>
2025-03-07 12:56:38 +0700

    update database

--- database/migrations/2025_03_03_095658_create_tbl_messages_table.php
+++ database/migrations/2025_03_03_095658_create_tbl_messages_table.php
@@ -1,25 +1,23 @@
 <?php
 
-// 2025_03_03_000008_create_messages_table.php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('tbl_messages', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_sender');
+            $table->unsignedBigInteger('id_receiver');
             $table->enum('status', ['removed', 'exist'])->default('exist');
             $table->text('content');
             $table->timestamps();
 
-            $table->foreign('id_user')
-                  ->references('id')
-                  ->on('tbl_users')
-                  ->onDelete('cascade');
+            // Ràng buộc khóa ngoại
+            $table->foreign('id_sender')->references('id')->on('tbl_users')->onDelete('cascade');
+            $table->foreign('id_receiver')->references('id')->on('tbl_users')->onDelete('cascade');
         });
     }
 
